fef51e45b11430870d1ad892f0a570b2e018e1c7
mabrursatori <mabrursatori@gmail.com>
2022-07-18 08:00:18 +0700

    bootstarp deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
